5279b57987528f5f7e23d8f5ee7381e69699111e
Denis <den88lit@mail.ru>
2021-03-23 16:33:11 +0300

    Added initDOMListeners

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,1 +1,25 @@
-export class Table extends ExcelComponent {}
+import { ExcelComponent } from '@core/ExcelComponent';
+
+export class Table extends ExcelComponent {
+  static className = `excel__table`;
+  toHTML() {
+    return `
+    <div class="row">
+    <div class="row-info"></div>
+    <div class="row-data">
+      <div class="column">A</div>
+      <div class="column">B</div>
+      <div class="column">C</div>
+    </div>
+  </div>
+  <div class="row">
+    <div class="row-info">1</div>
+    <div class="row-data">
+      <div class="cell selected" contenteditable="true">1assad</div>
+      <div class="cell" contenteditable="true">2sdsd</div>
+      <div class="cell" contenteditable="true">3asdas</div>
+    </div>
+  </div>
+    `;
+  }
+}
--- src/core/DomListener.js
+++ src/core/DomListener.js
@@ -1,1 +1,33 @@
-export class DomListener {}
+import { capitalize } from './utils';
+
+export class DomListener {
+  constructor($root, listeners = []) {
+    if (!$root) {
+      throw new Error('No $root provided for DomListener!');
+    } else {
+      this.$root = $root;
+      this.listeners = listeners;
+    }
+  }
+
+  initDOMListeners() {
+    this.listeners.forEach(listener => {
+      const method = getMethodName(listener);
+      if (!this[method]) {
+        throw new Error(
+          `Method ${method} is not implemented in ${this.name} Component.`
+        );
+      }
+      // Тоже самое что и addEventListener
+      this.$root.on(listener, this[method].bind(this));
+    });
+  }
+
+  removeDOMListeners() {
+    this.listeners.forEach(listener => {});
+  }
+}
+
+function getMethodName(eventName) {
+  return 'on' + capitalize(eventName);
+}
